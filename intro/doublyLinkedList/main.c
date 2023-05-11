7c80396d68d8105cff489c41432f7ce48aaf309f
sualah <sabdullai@acsint.io>
2023-05-11 14:27:22 +0000

    doubly linked list creation, insertion and deletion implemetation

--- intro/doublyLinkedList/main.c
+++ intro/doublyLinkedList/main.c
@@ -1,7 +1,111 @@
 #include <stdio.h>
+#include <main.h>
+#include <stdlib.h>
+#include <string.h>
+
+DoublyLinkedList *createList()
+{
+    DoublyLinkedList *list = malloc(sizeof(DoublyLinkedList));
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
+}
+
+Node *createNode(char *value)
+{
+    Node *node = malloc(sizeof(Node));
+    node->value = (char *)malloc(strlen(value) + 1);
+    strcpy(node->value, value);
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
+};
+
+void insert(char *value, DoublyLinkedList *list)
+{
+    Node *node = createNode(value);
+
+    if (list->head == NULL)
+    {
+        list->head = node;
+        list->tail = node;
+    }
+    else
+    {
+        node->prev = list->tail;
+        list->tail->next = node;
+        list->tail = node;
+    }
+}
+Node *find(struct DoublyLinkedList *list, char *value)
+{
+    Node *node = list->head;
+    while (node != NULL)
+    {
+        if (strcmp(node->value, value) == 0)
+        {
+            return node;
+        }
+        node = node->next;
+    }
+    return NULL;
+}
+
+void delete(char *value, DoublyLinkedList *list)
+{
+    Node *node = find(list, value);
+    if (node == list->head)
+    {
+        list->head = node->next;
+    }
+
+    if (node == list->tail)
+    {
+        list->tail = node->prev;
+    }
+    if (node->prev != NULL)
+    {
+        node->prev->next = node->next;
+    }
+
+    if (node->next != NULL)
+    {
+        node->next->prev = node->prev;
+    }
+
+    free(node->value);
+    free(node);
+}
+void printList(struct DoublyLinkedList *list)
+{
+    Node *node = list->head;
+    while (node != NULL)
+    {
+        printf("%s\n", node->value);
+        node = node->next;
+    }
+}
+
 
 int main()
 {
-    printf("Hello, World!\n");
+    DoublyLinkedList *list = createList();
+
+    insert(list, "Alice");
+    insert(list, "Bob");
+    insert(list, "Charlie");
+    insert(list, "David");
+    printf("List after insertions:\n");
+    printList(list);
+    Node *node = find(list, "Charlie");
+    if (node != NULL)
+    {
+        printf("Found node with value \"%s\"\n", node->value);
+    }
+    else
+    {
+        printf("Node not found\n");
+    }
+    delete (list, "Bob");
     return 0;
 }
--- intro/doublyLinkedList/main.h
+++ intro/doublyLinkedList/main.h
@@ -2,7 +2,25 @@
 #define _MAIN_H
 /**
  *
- * 
+ *
  */
+typedef struct Node
+{
+    char *value;
+    Node *next;
+    Node *prev;
+} Node;
 
+typedef struct DoublyLinkedList
+{
+    Node *head;
+    Node *tail;
+} DoublyLinkedList;
+
+DoublyLinkedList*  createList();
+Node* createNode(char *value);
+void insert(char *value, DoublyLinkedList *list);
+Node *find(struct DoublyLinkedList *list, char *value);
+void delete(char *value, DoublyLinkedList *list);
+void printList(struct DoublyLinkedList *list);
 #endif
