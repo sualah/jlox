8642954c6701e51b5eb06388690dc06782d96d48
sualah <sabdullai@acsint.io>
2023-08-06 23:03:36 +0000

    added interpreter

--- java-interpreter/src/com/interpreter/lox/Lox.java
+++ java-interpreter/src/com/interpreter/lox/Lox.java
@@ -10,8 +10,9 @@ import java.util.List;
 
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
-
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -29,6 +30,7 @@ public class Lox {
 
         // Indicate an error in the exit code.
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -47,15 +49,27 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        // stop if there was a syntax error.
+        if (hadError) return;
+
+        interpreter.interpret(expression);
+        //System.out.println(new AstPrinter().print(expression));
+//        // For now, just print the tokens.
+//        for (Token token : tokens) {
+//            System.out.println(token);
+//        }
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static  void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where,
@@ -63,5 +77,13 @@ public class Lox {
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static  void error (Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
